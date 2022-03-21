b3ad33929fde18db57658d0159930a004eef294d
alexandre <alexandre@mail.com>
2022-03-21 13:49:29 +0100

    create commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function App() {
+  // Déclare une nouvelle variable d'état, que l'on va appeler « count »
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count - 1)}>
+        Cliquez ici
+      </button>
+    </div>
+  );
+}
+export default App
